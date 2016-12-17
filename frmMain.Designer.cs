2ed00dd215e13d92cd4c7ae925f1b78a214a30c0
NVitanovic <x-team@live.com>
2016-12-17 05:30:53 +0100

    added the code

---
+++ frmMain.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace CIITLAB_UltraSonic2016
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tCheck = new System.Windows.Forms.Timer(this.components);
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.lblConsole = new System.Windows.Forms.Label();
+            this.lblConsole2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tCheck
+            // 
+            this.tCheck.Interval = 1;
+            this.tCheck.Tick += new System.EventHandler(this.tCheck_Tick);
+            // 
+            // pbLogo
+            // 
+            this.pbLogo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbLogo.Image = global::CIITLAB_UltraSonic2016.Properties.Resources.Logo;
+            this.pbLogo.Location = new System.Drawing.Point(0, 0);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(842, 469);
+            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbLogo.TabIndex = 0;
+            this.pbLogo.TabStop = false;
+            // 
+            // lblConsole
+            // 
+            this.lblConsole.AutoSize = true;
+            this.lblConsole.Location = new System.Drawing.Point(0, 0);
+            this.lblConsole.Name = "lblConsole";
+            this.lblConsole.Size = new System.Drawing.Size(58, 13);
+            this.lblConsole.TabIndex = 1;
+            this.lblConsole.Text = "CONSOLE";
+            // 
+            // lblConsole2
+            // 
+            this.lblConsole2.AutoSize = true;
+            this.lblConsole2.Location = new System.Drawing.Point(0, 13);
+            this.lblConsole2.Name = "lblConsole2";
+            this.lblConsole2.Size = new System.Drawing.Size(67, 13);
+            this.lblConsole2.TabIndex = 2;
+            this.lblConsole2.Text = "CONSOLE 2";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(842, 469);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblConsole2);
+            this.Controls.Add(this.lblConsole);
+            this.Controls.Add(this.pbLogo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMain";
+            this.Text = "UltraSonic Vitanovic i Zdravkovic";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tCheck;
+        private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.Label lblConsole;
+        private System.Windows.Forms.Label lblConsole2;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Windows;
+
+
+namespace CIITLAB_UltraSonic2016
+{
+    public partial class frmMain : Form
+    {
+        protected WebClient client;
+        protected Config configuration = null;
+        protected bool currentState; //ON - true, //OFF - false
+        protected Process videoApplication;
+        protected int currentDistance;
+        protected int numOfReads;
+        protected SerialPort serialClient;
+        protected const int RGBMAX = 255;
+        protected int lastDistance = 0;
+        public frmMain()
+        {
+            InitializeComponent();
+            XmlSerializer serializer = new XmlSerializer(typeof(Config));
+            StreamReader reader = new StreamReader("config.xml");
+            configuration = (Config)serializer.Deserialize(reader);
+            reader.Close();
+            client = new WebClient();
+            currentState = false;
+            videoApplication = new Process();
+            
+            videoApplication.StartInfo.FileName = configuration.applicationPath;
+            videoApplication.StartInfo.UseShellExecute = true;
+            currentDistance = 0;
+
+            if (!String.IsNullOrEmpty(configuration.logoPath))
+            {
+                if(File.Exists(configuration.logoPath))
+                    pbLogo.Load(configuration.logoPath);
+            }
+            if (!String.IsNullOrEmpty(configuration.backgroundColor))
+            {
+                ColorConverter c = new ColorConverter();
+                Color color = (Color)c.ConvertFromString(configuration.backgroundColor);
+                this.BackColor = Color.FromArgb(color.R, color.B, color.G);
+            }
+            if (configuration.useWifi)
+                client.OpenRead("http://" + configuration.serverAddress);
+            else
+            {
+                try
+                {
+                    serialClient = new SerialPort();
+                    serialClient.PortName = configuration.comPortSettings.port;
+                    serialClient.BaudRate = configuration.comPortSettings.baudRate;
+                    serialClient.Parity = Parity.None;
+                    serialClient.Handshake = Handshake.None;
+                    serialClient.DataBits = 8;
+                    serialClient.StopBits = StopBits.One;
+                    if(!serialClient.IsOpen)
+                        serialClient.Open();
+                    else
+                    {
+                        serialClient.Close();
+                        serialClient.Open();
+                    }
+                }
+                catch(Exception e)
+                {
+                    MessageBox.Show(e.InnerException + "\n\n\n" + e.Message + "\n\n\n" + e.StackTrace);
+                }
+            }
+            if (configuration.samplingTime <= 10)
+                configuration.samplingTime = 100;
+
+            tCheck.Interval = configuration.samplingTime;
+            tCheck.Enabled = true;
+
+            lblConsole2.ForeColor = lblConsole.ForeColor = InvertColor(this.BackColor);
+            this.WindowState = FormWindowState.Maximized;
+        }
+        Color InvertColor(Color ColourToInvert)
+        {
+            return Color.FromArgb(RGBMAX - ColourToInvert.R,
+              RGBMAX - ColourToInvert.G, RGBMAX - ColourToInvert.B);
+        }
+        void Clear()
+        {
+            numOfReads = currentDistance = 0;
+        }
+        private string ReadNumFromSerial()
+        {
+            byte[] buff = new byte[10];
+            char b = '0';
+            string val = "";
+
+            while (b != '\n')
+            {
+                if (b == -1)
+                    return null;
+                val += b;
+                b = (char)serialClient.BaseStream.ReadByte();
+            }
+            return val;
+        }
+        private void tCheck_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                int distance = 0;
+                //preskaci manje vrednosti od 10
+                //while(distance <= 10)
+                //{
+                    var response = "0";
+                    if (configuration.useWifi)
+                        response = client.DownloadString("http://" + configuration.serverAddress);
+                    else
+                    {
+                        serialClient.DiscardInBuffer();
+                        response = serialClient.ReadLine();
+                        //response = ReadNumFromSerial();
+                    }
+                        
+                    //var response = "40";
+                    if (String.IsNullOrEmpty(response))
+                        return;
+
+                    distance = Convert.ToInt32(response);
+                if (distance <= 10)
+                    distance = lastDistance;
+                lastDistance = distance;
+                if (configuration.debug)
+                {
+                    lblConsole2.Text = "RAW-B: " + distance.ToString() + "\nRAW-N: " + ReadNumFromSerial() + "\nREADS: " + numOfReads + " / " + configuration.numberOfSamples;
+                    if (distance < 10)
+                        lblConsole2.Text = "!!!TOO SMALL!!! =>" + distance.ToString();
+                }
+                else
+                    lblConsole2.Text = "";
+
+                if (numOfReads < configuration.numberOfSamples) //citamo vrednost vise puta
+                {
+                    currentDistance += distance;    //dodajemo vrednost
+                    numOfReads++;                   //povecavamo broj procitanih vrenosti
+                    return;
+                }
+
+                //racunamo srednju vrednost iz svih procitanih
+                distance = currentDistance / (configuration.numberOfSamples + 1);
+                if (configuration.debug)
+                    lblConsole.Text = "MIN: " + configuration.minDistance + " MAX: " + configuration.maxDistance + " CUR: " + distance.ToString() + " STATE: " + currentState.ToString();
+                else
+                    lblConsole.Text = "";
+                if(distance >= configuration.minDistance && distance <= configuration.maxDistance) //u okviru smo dozvoljene daljine
+                {
+                    if(!currentState) //ako nije upaljen upali ga
+                    {
+                        //MessageBox.Show("ON");
+                        currentState = true;
+                        //this.WindowState = FormWindowState.Minimized;
+                        numOfReads = currentDistance = 0; //reset distance
+                        videoApplication.Start();
+                    }
+                }
+                else //ako nema nikog u okviru zone
+                { 
+                    if(currentState) //ako je upaljen
+                    {
+                        //this.WindowState = FormWindowState.Maximized;
+                        //MessageBox.Show("OFF");
+                        currentState = false;
+                        numOfReads = currentDistance = 0; //reset distance
+                        videoApplication.Kill();
+                    }
+                }
+                Clear();
+
+            }
+           catch {}      
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                if (serialClient.IsOpen)
+                    serialClient.Close();
+            }
+            catch
+            {
+                serialClient.Close();
+                videoApplication.Kill();
+            }
+            
+        }
+    }
+}
